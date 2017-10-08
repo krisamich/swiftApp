8854f524cce88b06f9b42d965c02e54db6e55527
Kristen Michaud <krisamich@gmail.com>
2017-10-08 09:54:09 -0400

    Setup label to take sum of entries

--- swiftClassApp/ViewController.swift
+++ swiftClassApp/ViewController.swift
@@ -16,8 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonsButton(_ sender: Any) {
-        print(text1.text!)
-        print(text2.text!)
+        theLabel.text = "Answer is... \(Double(text1.text!)! + Double(text2.text!)!)"
     }
     
     
